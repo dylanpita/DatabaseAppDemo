f00dcfa4d1e6f2b27ef09bcfd2b859a54a680af2
Dylan <dylanpita007@users.noreply.github.com>
2019-10-17 16:23:12 -0400

    Add project files.

---
+++ Database/DB.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public static class DB
+    {
+        private static masterEntities myDb = new masterEntities();
+        public static IEnumerable<Product> GetProducts()
+        {
+            return myDb.Products.ToList();
+        }
+
+        public static Product GetProduct(int id)
+        {
+            return myDb.Products.Find(id);
+        }
+
+        public static void AddProduct(Product p)
+        {
+            try
+            {
+                myDb.Products.Add(p);
+                myDb.SaveChanges();
+
+                Console.WriteLine("Record Created in Products");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot Add Record to Products");
+            }
+        }
+
+        public static void DeleteCustomer(int id)
+        {
+            try
+            {
+                Customer c = myDb.Customers.Find(id);
+                myDb.Customers.Remove(c);
+                myDb.SaveChanges();
+
+                Console.WriteLine("Record Deleted.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot delete, record has foreign references");
+            }
+        }
+
+        public static void AddCustomer(Customer c)
+        {
+            try
+            {
+                myDb.Customers.Add(c);
+                myDb.SaveChanges();
+
+                Console.WriteLine("Record Created in Customer");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot Add Record in Customer");
+            }
+            
+        }
+
+        public static void AddSale(Sale s)
+        {
+            try
+            {
+                myDb.Sales.Add(s);
+                myDb.SaveChanges();
+
+                Console.WriteLine("Record Created in Sales");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot Add Record in Sales");
+            }
+        }
+
+        public static void UpdateCustomer(Customer c)
+        {   
+            if (c.CustomerFirstName != null)
+            {
+                myDb.Customers.Find(c.CustomerId).CustomerFirstName = c.CustomerFirstName;
+            }
+
+            if (c.CustomerLastName != null)
+            {
+                myDb.Customers.Find(c.CustomerId).CustomerLastName = c.CustomerLastName;
+            }
+            
+            myDb.SaveChanges();
+        }
+
+        public static List<Sale> GetSales()
+        { 
+            return myDb.Sales.ToList();
+        }
+    }
+}
---
+++ DatabaseAppDemo/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+
+namespace DatabaseAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myProducts = DB.GetProducts();
+
+            foreach (var product in myProducts)
+            {
+                Debug.WriteLine("ID: " + product.ProductId + "/nDesc: " + product.ProductDescription);
+            }
+
+            /* Adding Product */
+            Product p = new Product();
+            p.ProductDescription = "Vibrator";
+            p.ProductUpc = "123";
+
+            DB.AddProduct(p);
+
+            /* Adding Customer */
+            Customer c = new Customer();
+            c.CustomerFirstName = "Remi";
+            c.CustomerLastName = "Johnathan";
+
+            DB.AddCustomer(c);
+            
+            /* Adding Sale */
+            Sale s = new Sale();
+            s.SaleDate = DateTime.Now;
+            s.ProductId = 4;
+            s.CustomerId = 4;
+
+            DB.AddSale(s);
+            
+            /* Deleting Customer */
+            DB.DeleteCustomer(4);
+
+            /* Display Sales with Customer and Product */
+            var mySales = DB.GetSales();
+            foreach (var sale in mySales)
+            {
+                Console.WriteLine(sale.SaleId + " | " +sale.SaleDate+ " | " +sale.Customer.CustomerFirstName+ " " +sale.Customer.CustomerLastName+ " | " +sale.Product.ProductDescription);
+            }
+
+            /* Update Customer */
+            Customer c1 = new Customer();
+            c1.CustomerId = 3;
+            c1.CustomerFirstName = "Brendan";
+
+            DB.UpdateCustomer(c1);
+
+            
+        }
+    }
+}
